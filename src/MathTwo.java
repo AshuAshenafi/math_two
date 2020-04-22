3c520e3ef745c0ae832c1d1d275a1fc5cc503bb5
ashuashenafi <ashuashenafi@gmail.com>
2020-04-22 00:14:56 -0400

    first commit

---
+++ src/MathTwo.java
@@ -0,0 +1,9 @@
+public class MathTwo {
+    public static void main(String[] args) {
+        int mynumber = 2;
+        double myothernumber = 1.7938;
+
+        System.out.println("my number: " + mynumber);
+        System.out.println("my other number: " + myothernumber);
+    }
+}
